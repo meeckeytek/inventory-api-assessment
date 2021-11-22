74a6c809ea36e89df1b8cad8622af86b3afd08f1
Micheal Emmanuel (meeckeyTek) <69390115+meeckeytek@users.noreply.github.com>
2021-11-22 10:42:44 +0000

    new commit

--- routes/cartRoute.ts
+++ routes/cartRoute.ts
@@ -10,6 +10,9 @@ cartRoute.get('/', cartController.defaultMsg);
 //Adding to cart route
 cartRoute.get('/addToCart/:productId', isAuth, cartController.addToCart);
 
+//Remove from cart route
+cartRoute.post('/removeFromCart/:orderId', isAuth, cartController.removeFromCart);
+
 //Fetch cart items route
 cartRoute.get('/viewCart', isAuth, cartController.viewCart);
 
@@ -20,6 +23,6 @@ cartRoute.get('/checkOut', isAuth, cartController.checkOut);
 cartRoute.get('/viewOrderDetails/:orderId', isAuth, cartController.viewOrderDetails);
 
 //Fetching all orders route
-cartRoute.get('/allOrders', isAuth, cartController.viewAllOrders);
+cartRoute.post('/allOrders', isAuth, cartController.viewAllOrders);
 
 export default cartRoute;
